49f36f76a0fe33c0c4349901cadb80f5e0b673f1
HamzaMarz <106035227+HamzaMarz@users.noreply.github.com>
2025-03-15 23:54:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & filters.Regex("^(محادثة عامة|تحليل الأخبار الإقتصادية|الحكم الشرعي|أسعار العملات)$"), handle_section))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-    # run
+    # تشغيل البوت
     app.run_polling()
 
 if __name__ == "__main__":
